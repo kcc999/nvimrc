8f5433a6543d95667c244f0b770c5a54fdac41f5
kcc999 <karolc9991@gmail.com>
2025-01-28 11:57:33 +0100

    jump to def

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,5 +7,13 @@ require('mason-lspconfig').setup({
 		lsp.default_setup,
 	},
 })
+lsp.on_attach(function(client, bufnr)
+  -- Create a keymap for jump to definition
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {buffer = bufnr})
+  
+  -- Optionally, you can also map other related functions:
+  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {buffer = bufnr})
+  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', {buffer = bufnr})
+end)
 
 lsp.setup()
--- lua/karol/bindings.lua
+++ lua/karol/bindings.lua
@@ -5,6 +5,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- Telescope 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "fg", builtin.find_files, {})
+vim.keymap.set("n", "fh", builtin.live_grep, {})
 vim.keymap.set("n", "hg", builtin.git_files, {})
 
 vim.g.maplocalleader = ","
