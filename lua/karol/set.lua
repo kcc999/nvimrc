7243a5bdbcaf452a86efbe069c713e1778eba9e9
kcc999 <karolc9991@gmail.com>
2025-10-02 08:31:32 +0200

    m

--- lua/karol/set.lua
+++ lua/karol/set.lua
@@ -8,9 +8,26 @@ require('onedark').setup {
     style = 'darker'
 }
 vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme tokyonight-night]])
+vim.cmd([[colorscheme gruvbox]])
 vim.keymap.set('n', '<leader>ai', vim.lsp.buf.code_action, { desc = 'Perform Code Action (Autoimport)' })
+
+-- Format current buffer with LSP
+vim.keymap.set("n", "<leader>f", function()
+  vim.lsp.buf.format()
+end, { desc = "Format file" })
 
 -- init.lua
 vim.g["conjure#client#elixir#stdio#command"] = "iex -S mix"
 vim.g["conjure#client#elixir#stdio#prompt_pattern"] = "iex(%d+)>"
+
+-- Visual mode copy (Ctrl-C) and cut (Ctrl-X)
+vim.keymap.set("v", "<C-c>", '"+y')
+vim.keymap.set("v", "<C-x>", '"+d')
+
+-- Normal mode paste
+vim.keymap.set("n", "<C-v>", '"+p')
+
+-- Insert and command-line mode paste
+vim.keymap.set("i", "<C-v>", '<C-r>+')
+vim.keymap.set("c", "<C-v>", '<C-r>+')
+
